151129b8815a676ffdbbc8f68d1c090d77c61695
Theerapong Likhitsab <Theerapong5176@gmail.com>
2026-01-13 01:08:46 +0900

    add back comment out rds

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "ec2_instance_public_ip" {
   value       = module.ec2.ec2_public_ip
 }
 
+# output "rds_endpoint" {
+#   description = "RDS access endpoint (from rds module)"
+#   value       = module.rds.db_endpoint
+# }
+
 output "alb_dns_name" {
   description = "ALB DNS name (from alb module)"
   value       = module.alb.alb_dns_name
--- resources/rds/main.tf
+++ resources/rds/main.tf
@@ -92,12 +92,12 @@ resource "aws_security_group" "db_sg" {
 # }
 
 # Ingress Rules
-resource "aws_security_group_rule" "ec2_to_rds_ingress" {
-  type                     = "ingress"
-  description              = "Allow DB access from anywhere web servers"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.db_sg.id
-  source_security_group_id = var.web_sg_id
-} 
+# resource "aws_security_group_rule" "ec2_to_rds_ingress" {
+#   type                     = "ingress"
+#   description              = "Allow DB access from anywhere web servers"
+#   from_port                = 5432
+#   to_port                  = 5432
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.db_sg.id
+#   source_security_group_id = var.web_sg_id
+# } 
